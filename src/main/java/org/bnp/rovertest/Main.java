8b61910ceb4fb0add87408e0bd1cd5adc119ef03
ABOUGHIDA Emna <emna.aboughida@novencia.com>
2023-11-07 19:00:14 +0100

    Refactor the main method to reduce its complexity

--- src/main/java/org/bnp/rovertest/Main.java
+++ src/main/java/org/bnp/rovertest/Main.java
@@ -15,65 +15,50 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner fileScanner = new Scanner(new File(args[0]));
 
-        int finalX;
-        if (fileScanner.hasNextInt()) {
-            finalX = fileScanner.nextInt();
-        } else {
-            throw new InputMismatchException("The grid x is not an integer");
-        }
-
-        int finalY;
-        if (fileScanner.hasNextInt()) {
-            finalY = fileScanner.nextInt();
-        } else {
-            throw new InputMismatchException("The grid y is not an integer");
-        }
-
+        int finalX = getIntArgument(fileScanner);
+        int finalY = getIntArgument(fileScanner);
         fileScanner.nextLine();
 
         while (fileScanner.hasNext()) {
 
-            int x;
-            if (fileScanner.hasNextInt()) {
-                x = fileScanner.nextInt();
-            } else {
-                throw new InputMismatchException("The rover x position is not an integer");
-            }
-
-            int y;
-            if (fileScanner.hasNextInt()) {
-                y = fileScanner.nextInt();
-            } else {
-                throw new InputMismatchException("The rover y position is not an integer");
-            }
-
+            int x = getIntArgument(fileScanner);
+            int y = getIntArgument(fileScanner);;
             char orientation = fileScanner.next().charAt(0);
-
             Rover rover = new Rover(x, y, orientation);
-
             fileScanner.nextLine();
 
             String instructions = fileScanner.nextLine();
-            String[] instructionsArray = instructions.split("");
-
-            for (String instruction : instructionsArray) {
-                switch (instruction) {
-                    case LEFT -> {
-                        Command turnLeftCommand = Rover::turnToLeft;
-                        turnLeftCommand.executeCommand(rover);
-                    }
-                    case RIGHT -> {
-                        Command turnRightCommand = Rover::turnToRight;
-                        turnRightCommand.executeCommand(rover);
-                    }
-                    case MOVE -> {
-                        Command moveForwardCommand = r -> r.moveForward(finalX, finalY);
-                        moveForwardCommand.executeCommand(rover);
-                    }
-                }
-            }
+            moveRover(instructions, rover, finalX, finalY);
             System.out.println(rover);
         }
         fileScanner.close();
+    }
+
+    public static void moveRover(String instructions, Rover rover, int finalX, int finalY) {
+        String[] instructionsArray = instructions.split("");
+        for (String instruction : instructionsArray) {
+            switch (instruction) {
+                case LEFT -> {
+                    Command turnLeftCommand = Rover::turnToLeft;
+                    turnLeftCommand.executeCommand(rover);
+                }
+                case RIGHT -> {
+                    Command turnRightCommand = Rover::turnToRight;
+                    turnRightCommand.executeCommand(rover);
+                }
+                case MOVE -> {
+                    Command moveForwardCommand = r -> r.moveForward(finalX, finalY);
+                    moveForwardCommand.executeCommand(rover);
+                }
+            }
+        }
+    }
+
+    public static int getIntArgument(Scanner fileScanner) {
+        if (fileScanner.hasNextInt()) {
+            return fileScanner.nextInt();
+        } else {
+            throw new InputMismatchException("The passed argument is not an integer !");
+        }
     }
 }
